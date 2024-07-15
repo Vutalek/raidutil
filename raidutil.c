d55e133ee3b625f474090d7866ed559d2ec5761c
Vuta <mr.sergei02@mail.ru>
2024-07-15 13:41:53 +0300

    code

--- raidutil.c
+++ raidutil.c
@@ -45,9 +45,8 @@ void replace(char* disk, char* replacer)
     system(command);
     sprintf(command, "sudo mdadm /dev/%s --replace %s --with %s", array, disk, replacer);
     system(command);
-    sleep(2);
     sprintf(command, "sudo mdadm /dev/%s --remove %s", array, disk);
-    system(command);
+    while(system(command));
     free(array);
 }
 
